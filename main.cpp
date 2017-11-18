46f5fdf94296a8dfae488ffecab4880c7c12af7c
sergeyborov8 <sergborovkov@gmail.com>
2017-11-18 19:32:56 +0300

    added lab12

--- main.cpp
+++ main.cpp
@@ -1,42 +1,37 @@
-#include <stdio.h>
+#include <iostream>
+#include <functional>
+#include <future>
+#include <thread>
 #include <curl/curl.h>
 
-int main(int argc, char* argv[])
+void GetResponse(std::future<char*> &fut)
 {
-    if(argc < 2){
-        cout << "Нет параметра" << endl;
-        return 0;
-    }
-
-    char* name = argv[1];
     CURL *curl;
     CURLcode res;
-
+    char *url = fut.get();
     curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, name);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_easy_setopt(curl, CURLOPT_NOBODY, true);
-
-        res = curl_easy_perform(curl);
-
-        if(res != CURLE_OK){
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        }
-        else {
-            char *url = NULL;
-            curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-            if(url){
-                printf("Redirect to: %s\n", url);
-            }
-            
-            long response_code;
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-            printf("Response code: %ld\n", response_code);
-        }
-
-        curl_easy_cleanup(curl);
+    if (curl) {
+      curl_easy_setopt(curl, CURLOPT_URL, url);
+      curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
     }
+    res = curl_easy_perform(curl);
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    std::cout << "Response answer == " << http_code << '\n';
+}
+
+int main(int argc, char *argv[])
+{
+    char *url = argv[1];
+
+    std::promise<char*> prom;
+
+    std::future<char*> fut = prom.get_future();
+
+    std::thread th1 (GetResponse, std::ref(fut));
+
+    prom.set_value(url);
+
+    th1.join();
     return 0;
 }
