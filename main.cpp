0c29e36e180f9a48595dafe23eb42f063b3539f3
sergeyborov8 <sergborovkov@gmail.com>
2017-11-19 00:33:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,64 @@
-#include <iostream>
-#include <functional>
-#include <future>
-#include <thread>
-#include <curl/curl.h>
+#include <print.hpp>
+#include <cstdlib>
+#include <boost/program_options.hpp>
 
-void GetResponse(std::future<char*> &fut)
+
+namespace po = boost::program_options;
+
+void PrintFile(const std::string &path)
 {
-    CURL *curl;
-    CURLcode res;
-    char *url = fut.get();
-    curl = curl_easy_init();
-    if (curl) {
-      curl_easy_setopt(curl, CURLOPT_URL, url);
-      curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
-    }
-    res = curl_easy_perform(curl);
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-    std::cout << "Response answer == " << http_code << '\n';
+	std::string text;
+	while(std::cin >> text) {
+		std::ofstream out(path, std::ios_base::app);
+		print(text, out);
+		out << std::endl;
+	}
 }
 
-int main(int argc, char *argv[])
+/*void ReadFile(po::options_description &desc,
+							po::variables_map& vm)
 {
-    char *url = argv[1];
+	std::ifstream configfile("~/.config/demo.cfg");
+	vm = po::variables_map();
+	po::store(po::parse_config_file(configfile, desc), vm);
+	po::notify(vm);
+}*/
 
-    std::promise<char*> prom;
+int main(int argc, char** argv)
+{
+	std::string pathfile;
+	std::string name;
+	po::options_description desc("Allowed optins");
+	desc.add_options()
+			("output", po::value<std::string>(), "set name to logfile")
+			("variable", po::value<std::string>(&pathfile))
+			("name", po::value<std::string>(&name), "from config file")
+	;
 
-    std::future<char*> fut = prom.get_future();
+	po::variables_map vm;
+	std::string pathconf = std::getenv("HOME");
+	pathconf += "/.config/demo.cfg";
 
-    std::thread th1 (GetResponse, std::ref(fut));
+	std::ifstream configfile(pathconf);
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::store(po::parse_environment(desc,
+		[](const std::string &env_var)
+		{
+			return env_var == "DEMO_OUTPUT" ? "variable" : "";
+		}),
+		vm);
+	po::store(po::parse_config_file(configfile, desc), vm);
+	po::notify(vm);
 
-    prom.set_value(url);
-
-    th1.join();
-    return 0;
+	if (vm.count("output")) {
+		PrintFile(vm["output"].as<std::string>());
+	} else if (!pathfile.empty()) {
+		PrintFile(pathfile);
+	} else if (!name.empty()) {
+		PrintFile(name);
+	} else {
+		PrintFile("default.log");
+	}
+	configfile.close();
+	return 0;
 }
