7769f3d767bb08af23b1f4e9b4aa10be474e915f
sergeyborov8 <sergborovkov@gmail.com>
2017-11-19 01:39:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
 
     if (curl) {
 
-        curl_easy_setopt(curl, CURLOPT_URL, Url.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 3000);
